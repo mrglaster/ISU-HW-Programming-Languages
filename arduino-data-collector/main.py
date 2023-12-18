1d72652201db735d33ff7f8f9408b2ed9000992a
Egor Pristavka <pristavkaegor03@gmail.com>
2023-12-18 09:37:14 +0800

    Update main.py

--- arduino-data-collector/main.py
+++ arduino-data-collector/main.py
@@ -1,4 +1,6 @@
 import logging
+import time
+
 import zmq
 import csv
 import datetime
@@ -10,7 +12,7 @@ logger = logging.getLogger()
 
 
 def write_log(message):
-    logging.critical(f"{datetime.datetime.now()} : {message}")
+    logging.critical(f"{datetime.datetime.utcnow()} : {message}")
 
 
 def write_record(current_time, tag, value):
@@ -27,7 +29,7 @@ def main():
         clt.connect("tcp://192.168.0.102:5555")
         clt.subscribe('')
     except:
-        write_log(f"{datetime.datetime.now()} : Unable to connect to server")
+        write_log(f"Unable to connect to server")
         main()
 
     poller = zmq.Poller()
@@ -39,28 +41,31 @@ def main():
             if clt in socks and socks[clt] == zmq.POLLIN:
                 try:
                     message = clt.recv_string()
-                    current_time = datetime.datetime.now()
+                    current_time = datetime.datetime.utcnow()
                     print(message)
                     if len(message):
                         if len(message) == 1 and message not in available_keys:
-                            write_log(f"{current_time} : Unknown tag: {message}")
+                            write_log(f"Unknown tag: {message}")
                         elif len(message) == 1:
                             write_record(current_time, message, 'True')
                         else:
                             values_got = message.split(' ')
                             if values_got[0] not in available_keys:
-                                write_log(f"{current_time} : Unknown tag: {values_got[0]}")
+                                write_log(f"Unknown tag: {values_got[0]}")
                             else:
                                 write_record(current_time, values_got[0], values_got[1])
                     else:
-                        write_log(f"{current_time} : Message with length 0 received")
+                        write_log(f"Message with length 0 received")
                 except zmq.error.Again:
-                    write_log(f"{datetime.datetime.now()} : Timeout while receiving message")
+                    write_log(f"Timeout while receiving message")
+                except:
+                    write_log(f"It seems arduino is disconnected!")
+                    time.sleep(5)
             else:
-                write_log(f"{datetime.datetime.now()} : No connection to server. Retrying...")
+                write_log(f"No connection to device. Retrying...")
                 main()
     except KeyboardInterrupt:
-        write_log(f"{datetime.datetime.now()} : Keyboard Interrupt. Exiting...")
+        write_log(f"Keyboard Interrupt. Exiting...")
 
 
 if __name__ == '__main__':
